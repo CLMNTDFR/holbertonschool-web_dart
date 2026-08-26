c694c5547e44be8c8e89e4eb9ee3391c05d4fc2f
CLMNTDFR <deferclement59@gmail.com>
2026-08-26 11:54:21 +0200

    Refactor printRmCharacters function to simplify API call and improve error handling

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,17 +3,13 @@ import 'package:http/http.dart' as http;
 
 Future printRmCharacters() async {
   try {
-    String? url = 'https://rickandmortyapi.com/api/character';
+    var response = await http.get(
+      Uri.parse('https://rickandmortyapi.com/api/character'),
+    );
+    var data = json.decode(response.body);
 
-    while (url != null) {
-      var response = await http.get(Uri.parse(url));
-      var data = json.decode(response.body);
-
-      for (var character in data['results']) {
-        print(character['name']);
-      }
-
-      url = data['info']['next'];
+    for (var character in data['results']) {
+      print(character['name']);
     }
   } catch (error) {
     return 'error caught: $error';
