326efd6051398ba5a33c4b2100925dfdbbe70c99
CLMNTDFR <deferclement59@gmail.com>
2026-08-26 11:46:30 +0200

    4. Calcul Total Create a function calculateTotal() that calculates the total price of items for a certain user.
    Gets the user data by calling the provided functionfetchUserData()
    Gets the user orders by calling the provided function fetchUserOrders(id) which accepts one argument : ID of the user
    Gets the products price by calling the provided function fetchProductPrice(product) which accepts one argument : product name
    Returns the total price of items
    Must use try-catch
    If an error occurs the function should return -1

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+Future calculateTotal() async {
+  try {
+    var userData = await fetchUserData();
+    var user = json.decode(userData);
+    var ordersData = await fetchUserOrders(user['id']);
+    var orders = json.decode(ordersData) as List;
+
+    double total = 0;
+    for (var product in orders) {
+      var priceData = await fetchProductPrice(product);
+      total += json.decode(priceData);
+    }
+    return total;
+  }
+}
